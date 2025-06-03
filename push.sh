395781b549a38236dc8341c1bd1c86c7bd9a9edf
mouhameddiouf <seydiahmedelcheikh@gmail.com>
2025-06-03 06:52:46 +0000

    On s'attaque au monitoring avec prometheus

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "AWS TERRAFORM TERMINÉ"
+git commit -m "On s'attaque au monitoring avec prometheus"
 git push --force
